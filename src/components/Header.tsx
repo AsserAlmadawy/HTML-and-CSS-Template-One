7e0899e1eea3c9d1ed5ee29e34d3fe6b27f322f8
Asser Almadawy <asser.almadawy@hotmail.com>
2024-01-29 23:19:03 +0200

    HTML and CSS Template One

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import Image from "next/image";
 import logo from "@/assets/images/logo.png";
 import "@/styles/Header.css";
 
-const Header: React.FC = () => (
+const Header: React.FC = () => {
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  return (
   <div className="header">
     <div className="header-image">
       <Image src={logo} alt="logo" className="logo" />
     </div>
-    <div className="header-lines">
+    <div className="header-lines" onMouseOver={() => menuRef.current!.style.display = "block"}>
       <div className="line"></div>
       <div className="line two"></div>
       <div className="line"></div>
     </div>
-    <div className="header-menu">
+    <div className="header-menu" ref={menuRef} onMouseOver={() => menuRef.current!.style.display = "block"} onMouseLeave={() => menuRef.current!.style.display = "none"}>
       <ul>
         <li>Services</li>
         <li>Portfolio</li>
@@ -22,6 +27,7 @@ const Header: React.FC = () => (
       </ul>
     </div>
   </div>
-);
+  );
+};
 
 export default Header;
