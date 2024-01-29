17491f82dd3ab1ac92f8787edb623f8d2217c30f
Asser Almadawy <asser.almadawy@hotmail.com>
2024-01-30 01:43:56 +0200

    HTML and CSS Template One

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "@/styles/SectionTitle.css";
 
-const SectionTitle: React.FC = () => {
-  return <div>About</div>;
-};
+interface SectionTitleProps {
+  lightText: string;
+  blackText: string;
+}
+
+const SectionTitle: React.FC<SectionTitleProps> = props => (
+  <>
+    <div className="light-text">{props.lightText}ld</div>
+    <div className="black-text">{props.blackText}</div>
+  </>
+);
 
 export default SectionTitle;
